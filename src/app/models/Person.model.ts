2f1d1c565732d04e37a17336aa4248c36f6519e9
Rob Parker <parky128@googlemail.com>
2018-08-26 21:09:15 +0100

    add person model

---
+++ src/app/models/Person.model.ts
@@ -0,0 +1,16 @@
+export class Person {
+  birthday?: string;
+  known_for_department?: boolean;
+  deathday?: string;
+  id?: number;
+  name?: string;
+  also_known_as?: Array<string>;
+  gender?: number;
+  biography?: string;
+  popularity?: number;
+  place_of_birth?: number;
+  profile_path?: string;
+  adult?: boolean;
+  imdb_id?: string;
+  homepage?: string;
+}

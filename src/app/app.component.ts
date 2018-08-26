6ea7ba9ee5407897a94e6d5582c0ea6ffc53ce0f
Rob Parker <parky128@googlemail.com>
2018-08-26 15:42:20 +0100

    change to using sass for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { TmdbSearchService } from './services/tmdb-search.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'my-tmdb-app'; //translate this?!

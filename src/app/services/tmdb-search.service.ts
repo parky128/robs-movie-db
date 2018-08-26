5b1d85edd39fc0449d4240fa27b21864a955339d
Rob Parker <parky128@googlemail.com>
2018-08-26 21:08:54 +0100

    removed commented out lines

--- src/app/services/tmdb-search.service.ts
+++ src/app/services/tmdb-search.service.ts
@@ -7,16 +7,10 @@ import { HttpClient } from '@angular/common/http';
 
 export class TmdbSearchService {
 
-  //public apiConfig: ApiConfigModel;
-
   constructor(private http: HttpClient) {
   }
 
   public performSearch = (searchTerm: string) => {
     return this.http.get('https://api.themoviedb.org/3/search/multi?api_key=7b157a93d615cd3ca2b3312055fa550c&query=' + searchTerm);
-      // .toPromise()
-      // .then((apiConfigResponse: ApiConfigModel) => {
-      //   this.apiConfig = apiConfigResponse;
-      // });
   }
 }

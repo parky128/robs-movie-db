49506bedffd39f3ad1657fb7202b0d98705d5500
Rob Parker <parky128@googlemail.com>
2018-08-26 21:07:25 +0100

    remove redundant module includes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import {
-  MatToolbarModule,
-  MatAutocompleteModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatIconModule
-} from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatToolbarModule} from "@angular/material";
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ApiConfigService } from './services/api-config.service';
 import { SearchModule } from './search/search.module';
-import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+import { MovieModule } from './movie/movie.module';
+import { TvModule } from './tv/tv.module';
+import { PersonModule } from './person/person.module';
 
 export function appConfigProvider(apiConfigService: ApiConfigService) {
   return () => apiConfigService.getApiConfig();
@@ -24,22 +19,19 @@ export function appConfigProvider(apiConfigService: ApiConfigService) {
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatToolbarModule,
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatIconModule,
     AppRoutingModule,
-    SearchModule
+    MatToolbarModule,
+    SearchModule,
+    HomeModule,
+    MovieModule,
+    TvModule,
+    PersonModule
   ],
 
   providers: [

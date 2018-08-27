d1d39335255da2f9f2151fb6ac7099bded0a994b
Rob Parker <parky128@googlemail.com>
2018-08-27 09:26:45 +0100

    add further data models

---
+++ src/app/models/Movie.model.ts
@@ -0,0 +1,29 @@
+import { Genre } from './Genre.model';
+import { ProductionCompany } from './ProductionCompany.model';
+import { Country } from './Country.model';
+import { SpokenLanguage } from './SpokenLanguage.model';
+
+export class Movie {
+  adult?: boolean;
+  backdrop_path?: string;
+  belongs_to_collection?: any;
+  budget?: number;
+  genres?: Array<Genre>;
+  homepage?: string;
+  id?: number;
+  imdb_id?: string;
+  original_language?: string;
+  original_title?: string;
+  overview?: string;
+  popularity?: number;
+  poster_path?: string;
+  production_companies?: Array<ProductionCompany>;
+  production_countries?: Array<Country>;
+  spoken_languges?: Array<SpokenLanguage>;
+  status?: string;
+  tagline?: string;
+  title?: string;
+  video?: true;
+  vote_average?: number;
+  vote_count?: number;
+}

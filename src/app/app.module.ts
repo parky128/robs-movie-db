d87fbbc832b0f64873a56c2cb6b8e570be8e20af
Rob Parker <parky128@googlemail.com>
2018-09-01 08:06:13 +0100

    configure app to use custom http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material';
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS  } from '@angular/common/http';
+import { MatToolbarModule, MatProgressSpinnerModule } from '@angular/material';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ApiConfigService } from './services/api-config.service';
+import { ApiConfigService } from './services/api-config/api-config.service';
 import { SearchModule } from './search/search.module';
 import { LanguageModule } from './language/language.module';
 import { HomeModule } from './home/home.module';
 import { MovieModule } from './movie/movie.module';
 import { TvModule } from './tv/tv.module';
 import { PersonModule } from './person/person.module';
+import { APIRequestInterceptorService } from './services/api-request-interceptor/api-request-interceptor.service';
+
 export function appConfigProvider(apiConfigService: ApiConfigService) {
   return () => apiConfigService.getApiConfig();
 }
@@ -29,6 +31,7 @@ export function appConfigProvider(apiConfigService: ApiConfigService) {
     HttpClientModule,
     AppRoutingModule,
     MatToolbarModule,
+    MatProgressSpinnerModule,
     SearchModule,
     HomeModule,
     MovieModule,
@@ -45,7 +48,8 @@ export function appConfigProvider(apiConfigService: ApiConfigService) {
   ],
   exports: [TranslateModule],
   providers: [
-    { provide: APP_INITIALIZER, useFactory: appConfigProvider, deps: [ApiConfigService], multi: true }
+    { provide: APP_INITIALIZER, useFactory: appConfigProvider, deps: [ApiConfigService], multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: APIRequestInterceptorService, multi: true },
   ],
   bootstrap: [AppComponent]
 })

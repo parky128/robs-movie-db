5773c54e4433815443e41ee59fcfe13e8cf272c3
parky128 <parky128@googlemail.com>
2018-09-02 17:09:26 +0100

    add home route resolvers for trending results display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { AppRouteMoviesResolver } from './app-routing-movies.resolver';
+import { AppRouteShowsResolver } from './app-routing-shows.resolver';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    resolve: {
+      trendingMovies: AppRouteMoviesResolver,
+      trendingShows: AppRouteShowsResolver
+    },
+    runGuardsAndResolvers: 'always'
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AppRouteMoviesResolver, AppRouteShowsResolver]
 })
 
 export class AppRoutingModule { }

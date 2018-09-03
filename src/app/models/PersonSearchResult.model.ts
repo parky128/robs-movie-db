a1401b378c21ecf678c24d993bf582a9f1027090
parky128 <robert.parker@weareact.com>
2018-09-03 13:44:42 +0100

    make properties optional

--- src/app/models/PersonSearchResult.model.ts
+++ src/app/models/PersonSearchResult.model.ts
@@ -2,11 +2,11 @@ import { MovieSearchResult } from './MovieSearchResult.model';
 import { TVSearchResult } from './TVSearchResult.model';
 
 export class PersonSearchResult {
-  profile_path: string;
-  adult: boolean;
-  id: number;
-  media_type: string;
-  known_for: Array<MovieSearchResult | TVSearchResult>;
-  name: string;
-  popularity: number;
+  profile_path?: string;
+  adult?: boolean;
+  id?: number;
+  media_type?: string;
+  known_for?: Array<MovieSearchResult | TVSearchResult>;
+  name?: string;
+  popularity?: number;
 }
